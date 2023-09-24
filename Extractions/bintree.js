f9a7ed5497206def11813177e1c5a7cc396b3ac0
mozow01 <78300414+mozow01@users.noreply.github.com>
2023-09-25 01:21:14 +0200

    Add files via upload

---
+++ Extractions/bintree.js
@@ -0,0 +1,26 @@
+// A treeLength függvény összeszámolja a leveleket
+function treeLength(tree) {
+    if (tree.tag === 'leaf') {
+        return 1;
+    }
+    else {
+        return treeLength(tree.left) + treeLength(tree.right);
+    }
+}
+// tree1: leaf
+//
+// tree2: leaf leaf
+//           \/
+//          root
+//
+// tree3: leaf leaf
+//           \/
+//          node  leaf  
+//              \/
+//             root
+var tree1 = { tag: 'leaf' };
+var tree2 = { tag: 'node', left: tree1, right: tree1 };
+var tree3 = { tag: 'node', left: tree2, right: tree1 };
+// kilog :)
+var l2 = treeLength(tree2);
+console.log(l2);
---
+++ Extractions/bintree.ts
@@ -0,0 +1,36 @@
+
+// Címke nélküli bináris fák típusa, "levélhossz" függvény
+//
+// TS-ben nincs induktív típus, de van olyan, hogy 
+// hasonló objektumok típusa. Ilyet csinálunk:
+type Tree =
+  | { tag: 'leaf' }
+  | { tag: 'node'; left: Tree; right: Tree };
+
+// A treeLength függvény összeszámolja a leveleket
+function treeLength(tree: Tree): number {
+  if (tree.tag === 'leaf') {
+    return 1; 
+  } else {
+    return treeLength(tree.left) + treeLength(tree.right);
+  }
+}
+
+// tree1: leaf
+//
+// tree2: leaf leaf
+//           \/
+//          root
+//
+// tree3: leaf leaf
+//           \/
+//          node  leaf  
+//              \/
+//             root
+const tree1: Tree = { tag: 'leaf' };
+const tree2: Tree = { tag: 'node', left: tree1, right: tree1 };
+const tree3: Tree = { tag: 'node', left: tree2, right: tree1 };
+
+// kilog :)
+const l2 = treeLength(tree2);
+console.log(l2); 
